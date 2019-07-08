996c4e4dd533f4392a39f7a22d0c1684bb454578
vit0r <vitornascimentoaraujo@gmail.com>
2019-07-07 21:30:39 -0300

    create enviroment docker

---
+++ app/src/main/java/com/springbootho/herokuapp/appes/AppEsApplication.java
@@ -0,0 +1,13 @@
+package com.springbootho.herokuapp.appes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppEsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppEsApplication.class, args);
+	}
+
+}
---
+++ app/src/test/java/com/springbootho/herokuapp/appes/AppEsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootho.herokuapp.appes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppEsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
